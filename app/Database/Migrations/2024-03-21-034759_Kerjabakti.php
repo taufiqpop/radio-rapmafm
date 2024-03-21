9657fd55435bf50ca8dc95a0da13e6c9e654b9e6
taufiqpop99 <taufiqpop999@gmail.com>
2024-03-21 10:49:42 +0700

    database piket

--- app/Database/Migrations/2024-03-21-034759_Kerjabakti.php
+++ app/Database/Migrations/2024-03-21-034759_Kerjabakti.php
@@ -8,11 +8,36 @@ class Kerjabakti extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'key' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'value' => [
+                'type'       => 'TEXT',
+            ],
+            'created_at' => [
+                'type'       => 'DATETIME',
+            ],
+            'updated_at' => [
+                'type'       => 'DATETIME',
+            ],
+            'deleted_at' => [
+                'type'       => 'DATETIME',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('kerjabakti');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('kerjabakti');
     }
 }
