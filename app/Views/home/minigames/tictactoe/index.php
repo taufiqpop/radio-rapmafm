50cf0e5c1397d0d51c22d1dd9010eb829fbe6aa6
taufiqpop99 <taufiqpop999@gmail.com>
2024-04-20 10:11:46 +0700

    test react

--- app/Views/home/minigames/tictactoe/index.php
+++ app/Views/home/minigames/tictactoe/index.php
@@ -1,13 +1,39 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Rapma FM | Tic Tac Toe</title>
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="UTF-8" />
+  <title>Belajar React</title>
+
+  <!-- React Library -->
+  <script src="https://unpkg.com/react@18/umd/react.development.js"></script>
+  <script src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"></script>
+
+  <!-- Don't use this in production: -->
+  <script src="https://unpkg.com/@babel/standalone/babel.min.js"></script>
+</head>
+
+<body>
+  <div id="root"></div>
+
+  <script type="text/babel">
+    function HomePage () {
+        const [likes, setLikes] = React.useState(0);
+
+        function HandleClick () {
+            setLikes(likes + 1)
+        }
+    
+        return (
+            <>
+            <button onClick={HandleClick}>Like ({likes})</button>
+            </>
+        );
+    }
+    const container = document.getElementById('root');
+    const root = ReactDOM.createRoot(container);
+    root.render(<HomePage />);
+    </script>
+</body>
+
 </html>
